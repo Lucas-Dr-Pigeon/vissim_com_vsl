7cd36bac8542670ba21a98ff2b3386103b889efc
Lucas-Dr-Pigeon <63327766+Lucas-Dr-Pigeon@users.noreply.github.com>
2021-08-20 00:57:44 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def _trial1():
     vis = InitVissim()
     SyncTime()
     # test.test_create_networks()
-    vis.LoadNet(r'C:\Users\mini_lab\PycharmProjects\\vis_project_p_may_05_21_01\\networks\example\\test1.inp')
+    vis.LoadNet(r'C:\Users\Lucas_Pigeon\PythonProjects\\vissim_com_vsl\\networks\example\\test1.inp')
     sim = vis.Simulation
     net = vis.Net
     eval = vis.Evaluation
@@ -15,8 +15,11 @@ def _trial1():
     print(dir(eval))
 
 if __name__ == "__main__":
-    dir = r'C:\Users\mini_lab\PycharmProjects\\vis_project_p_may_05_21_01\\networks\example\\test_f001.inp'
+    dir = r'C:\Users\Pigeon_Zuo\PythonProjects\\vissim_com_vsl\\networks\example\\test3_ramps.inp'
+
     vis = OpenVissim(dir)
-    vis.runSimulation(_totalPeriod = 600)
+    vis.runSimulation(_totalPeriod=300)
+
+
 
 
